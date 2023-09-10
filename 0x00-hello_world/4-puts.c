c69d5a5fbc582aa305218a6c5d8e9eeccbfbf941
Realtunny <akinseyetunde2018@gmail.com>
2023-09-10 10:16:12 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a main function thats prints out words
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
