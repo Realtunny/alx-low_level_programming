5a17ba9b7a05621ac30e88f8ee9c3731e0e8afb8
Realtunny <akinseyetunde2018@gmail.com>
2023-10-15 09:28:43 -0700

    updated agrc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - print name
+ * @argc: argument count
+ * @argv: argument vector for values
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", *argv);
+	return (0);
+}
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - prints the number of arguments.
+ * @argc: argument count
+ * @argv: Argument vector for values
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	argc--;
+	printf("%d\n", argc);
+	return (0);
+}
